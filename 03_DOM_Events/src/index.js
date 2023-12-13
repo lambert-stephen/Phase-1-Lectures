d91e1777af937e774488dce31acf0d8a8657e420
stephenlambertfs <stephenolambert@gmail.com>
2023-12-13 11:46:43 -0700

    finished solution code

--- 03_DOM_Events/src/index.js
+++ 03_DOM_Events/src/index.js
@@ -1,56 +1,92 @@
-function renderHeader(){
-    return document.querySelector('h1').textContent = bookStore.name
-}
+document.addEventListener("DOMContentLoaded", ()=>{
+    function renderHeader(){
+        return document.querySelector('h1').textContent = bookStore.name
+    }
 
-console.log(renderHeader())
+    console.log(renderHeader())
 
-function renderFooter(){
-    const footerDivs = document.querySelectorAll('footer div')
-    console.log(footerDivs)
-    footerDivs[0].textContent = bookStore.name
-    footerDivs[1].textContent = bookStore.address
-    footerDivs[2].textContent = bookStore.hours
-    return footerDivs
-}
-console.log(renderFooter())
-//* 1. use a forEach to iterate over BookStore inventory.
-//  2. Pass the forEach an anonymous callback that takes the inventory data and creates an li, with an h3 tag, 2 p tags, and image tag.
-// 3. Add the cardData content to the tags.
-// 4. Append li to the DOM through the ul with the id of book-list
-// bookStore.inventory.forEach((cardData) => {
-//     console.log(cardData)
-//     const li = document.createElement('li')
-//     const h3 = document.createElement("h3")
-//     const pAuthor = document.createElement('p')
-//     const pPrice = document.createElement('p')
-//     const image = document.createElement('img')
-//     const btn = document.createElement('button')
-//     h3.textContent = cardData.title
-//     pAuthor.textContent = cardData.author
-//     pPrice.textContent = `$ ${cardData.price}`
-//     btn.textContent = "Delete"
-//     image.src = cardData.imageUrl
-//     li.className = 'list-li'
-//     li.append(h3, pAuthor, pPrice, image, btn)
-//     document.querySelector('#book-list').append(li)
-// })
+    function renderFooter(){
+        const footerDivs = document.querySelectorAll('footer div')
+        console.log(footerDivs)
+        footerDivs[0].textContent = bookStore.name
+        footerDivs[1].textContent = bookStore.address
+        footerDivs[2].textContent = bookStore.hours
+        return footerDivs
+    }
+    console.log(renderFooter())
+    //* 1. use a forEach to iterate over BookStore inventory.
+    //  2. Pass the forEach an anonymous callback that takes the inventory data and creates an li, with an h3 tag, 2 p tags, and image tag.
+    // 3. Add the cardData content to the tags.
+    // 4. Append li to the DOM through the ul with the id of book-list
+    // bookStore.inventory.forEach((cardData) => {
+    //     console.log(cardData)
+    //     const li = document.createElement('li')
+    //     const h3 = document.createElement("h3")
+    //     const pAuthor = document.createElement('p')
+    //     const pPrice = document.createElement('p')
+    //     const image = document.createElement('img')
+    //     const btn = document.createElement('button')
+    //     h3.textContent = cardData.title
+    //     pAuthor.textContent = cardData.author
+    //     pPrice.textContent = `$ ${cardData.price}`
+    //     btn.textContent = "Delete"
+    //     image.src = cardData.imageUrl
+    //     li.className = 'list-li'
+    //     li.append(h3, pAuthor, pPrice, image, btn)
+    //     document.querySelector('#book-list').append(li)
+    // })
 
-//* Refactor to make the anonymous callback its own function so it can be reused later. 
-const renderBookCard = (cardData) => {
-    console.log(cardData)
-    const li = document.createElement('li')
-    const h3 = document.createElement("h3")
-    const pAuthor = document.createElement('p')
-    const pPrice = document.createElement('p')
-    const image = document.createElement('img')
-    const btn = document.createElement('button')
-    h3.textContent = cardData.title
-    pAuthor.textContent = cardData.author
-    pPrice.textContent = `$ ${cardData.price}`
-    btn.textContent = "Delete"
-    image.src = cardData.imageUrl
-    li.className = 'list-li'
-    li.append(h3, pAuthor, pPrice, image, btn)
-    document.querySelector('#book-list').append(li)
-}
-bookStore.inventory.forEach(renderBookCard)
+    //* Refactor to make the anonymous callback its own function so it can be reused later. 
+    const renderBookCard = (cardData) => {
+        console.log(cardData)
+        const li = document.createElement('li')
+        const h3 = document.createElement("h3")
+        const pAuthor = document.createElement('p')
+        const pPrice = document.createElement('p')
+        const image = document.createElement('img')
+        const btn = document.createElement('button')
+        h3.textContent = cardData.title
+        pAuthor.textContent = cardData.author
+        pPrice.textContent = `$ ${cardData.price}`
+        btn.textContent = "Delete"
+        image.src = cardData.imageUrl
+        li.className = 'list-li'
+        li.append(h3, pAuthor, pPrice, image, btn)
+        document.querySelector('#book-list').append(li)
+        btn.addEventListener("click", (e) => {
+            // li.remove()
+            // e.target.parentElement.remove()
+            console.log(e.target.parentElement)
+            console.log(e.target.parentNode)
+            e.target.parentNode.remove()
+        })
+    }
+    bookStore.inventory.forEach(renderBookCard)
+
+    function log(event){
+
+        console.log(event)
+    }
+    document.querySelector("label").addEventListener("click", (e) => {
+        console.log(e)
+    })
+
+    const form = document.querySelector("#book-form")
+    form.addEventListener("submit", (event) => {
+        console.log(event)
+        event.preventDefault()
+        const book = {
+            title: event.target["title"].value,
+            author: event.target["author"].value,
+            price: event.target["book-price"].value,
+            imageUrl: event.target.imageUrl.value,
+            inventory: event.target.inventory.value,
+            reviews: []
+        }
+        console.log(book)
+        debugger;
+        renderBookCard(book)
+
+    })
+
+})
--- 03_DOM_Events/src/review.js
+++ 03_DOM_Events/src/review.js
@@ -1,14 +1,50 @@
 //Review Arrays
-
-
+const arr = [10, 20, 15, 0, 1]
+arr.push(25, 30)
+console.log(arr)
+arr.pop()
+console.log(arr)
 //push and pop from an array
-
-
 //destruture an array with spread operator
-
-
+const newArray = [...arr]
+console.log(newArray)
 //filter an array
+const filterArray = newArray.filter((number) => {
+    console.log(number)
+    if(number > 10){
+        console.log(number)
+        return true
+    }
+    // else{
+    //     return false
+    // }
+    return false
+})
+console.log(filterArray)
 
+const students = {
+    First_Name: "John",
+    lastName: "Doe",
+    "age": 25,
+    concentration: "Software Engineering",
+    phases: ["Javacscript", "React", "Python", "Flask"],
+    func: (data) => console.log(data)
+
+}
+console.log(students.firstname)
+console.log(students["age"])
+students.firstname = "Stephen"
+console.log(students)
+console.log(Object.values(students))
+console.log(Object.keys(students))
+
+const studentUpdate = []
+for(student in students){
+    console.log(students[student])
+    console.log(student)
+    studentUpdate.push(students[student])
+}
+console.log(studentUpdate)
 
 //select an element from the dom using querySelector
 
