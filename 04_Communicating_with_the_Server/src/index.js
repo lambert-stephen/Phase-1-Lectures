bcdd9fea9172184a2a98906df513fa385c5e2d91
Stephen Lambert <97977357+lambert-stephen@users.noreply.github.com>
2023-12-14 13:49:33 -0700

    Update index.js

--- 04_Communicating_with_the_Server/src/index.js
+++ 04_Communicating_with_the_Server/src/index.js
@@ -1,11 +1,58 @@
 document.addEventListener("DOMContentLoaded", ()=> {
 
-    
-    function renderHeader(){
+    // fetch('http://localhost:4000/books')
+    // .then((res)=> res.json())
+    // .then((book) => book.forEach(renderBookCard))
+    // // .then((book) => renderBookCard(book))
+    // .catch(e => console.log(e))
+
+    // fetch('http://localhost:4000/stores/1')
+    // .then((response)=> response.json())
+    // .then(store => {
+    //     renderHeader(store)
+    //     renderFooter(store)
+    // })
+    //fetching diffrent urls
+    function fetchResource(url){
+        return fetch(url)
+        .then(response => response.json())
+    }
+    fetchResource("http://localhost:4000/stores")
+    .then(store => {
+        const store_list = document.createElement("ul")
+        const footer = document.querySelector("#f1")
+        footer.append(store_list)
+        store_list.className = "store-menu"
+        store.forEach((s)=> {
+            console.log(s)
+            const store_li = document.createElement("li")
+            store_li.textContent = s.name
+            store_list.append(store_li)
+            store_li.addEventListener("click", ()=>{
+                renderFooter(s)
+                renderHeader(s)
+            })
+        })
+        renderFooter(store)
+        renderHeader(store)
+    })
+    .catch(e => console.error(e))
+
+    fetchResource("http://localhost:4000/books")
+    .then(book => book.forEach(renderBookCard))
+    .catch(e => console.error(e))
+
+
+
+
+
+
+
+    function renderHeader(bookStore){
         return document.querySelector('h1').textContent = bookStore.name
     }
     
-    function renderFooter(){
+    function renderFooter(bookStore){
         const footerDivs = document.querySelectorAll('footer div')
         console.log(footerDivs)
         footerDivs[0].textContent = bookStore.name
@@ -32,7 +79,7 @@ document.addEventListener("DOMContentLoaded", ()=> {
         document.querySelector('#book-list').append(li)
         btn.addEventListener('click', (e) => {li.remove()})
     }
-    bookStore.inventory.forEach(renderBookCard)
+    // bookStore.inventory.forEach(renderBookCard)
     
     document.querySelector("#book-form").addEventListener("submit", (e) => {
         e.preventDefault()
